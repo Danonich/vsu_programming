4b5f32dd3416bdc412156e95fcea785aaf9ffa89
Danonich <49477225+Danonich@users.noreply.github.com>
2019-09-23 14:01:18 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,12 +1,9 @@
-a = float(input ('a: '))
+a = float(input('a: '))
 b = float(input('b: '))
 c = float(input('c: '))
 f = float(input('f: '))
 d = float(input('d: '))
 g = f - d
-
-
-
 
 if g:
     print ('ne delit') 
