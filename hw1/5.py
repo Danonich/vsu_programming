a2766f6ca44938d1fdb2c1376730a51d9687ab9c
Danonich <49477225+Danonich@users.noreply.github.com>
2019-10-12 12:55:28 +0300

    Create 5.py

---
+++ hw1/5.py
@@ -0,0 +1,13 @@
+x = float(input('x: '))
+y = float(input('y: '))
+
+if x > 0 and y > 0:
+    print('I')
+elif x < 0 and y > 0:
+    print('II')
+elif x < 0 and y < 0:
+    print ('III')
+elif x > 0 and y < 0:
+    print ('IV')
+else
+    print('s osi')
