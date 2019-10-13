b5d7e907307230370080784c75181cab5ea770a9
Danonich <49477225+Danonich@users.noreply.github.com>
2019-10-13 11:14:26 +0300

    Create 1.py

---
+++ hw3/1.py
@@ -0,0 +1,17 @@
+import re
+
+Ex = input('Example: ')
+s = ''.join(re.findall(r'[+, - ,*,/,%,**]', Ex))
+index = Ex.find(s)
+x, y = Ex.split(s)
+x, y = float(x), float(y)
+
+op = {
+'+': x + y,
+'-': x - y,
+'/': x / y,
+'*': x * y,
+'//': x // y,
+'**': x ** y}
+print(op[s])
+
