7bacaba423439f778731f6906dba5897812c8cb9
Danonich <49477225+Danonich@users.noreply.github.com>
2019-10-19 10:11:45 +0300

    Update 1.py

--- hw3/1.py
+++ hw3/1.py
@@ -1,11 +1,9 @@
 import re
-
 Ex = input('Example: ')
 s = ''.join(re.findall(r'[+, - ,*,/,%,**]', Ex))
 index = Ex.find(s)
 x, y = Ex.split(s)
 x, y = float(x), float(y)
-
 op = {
 '+': x + y,
 '-': x - y,
