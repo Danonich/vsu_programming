d94beac1af9e91d0f493fe4605310eed4a08325f
Danonich <49477225+Danonich@users.noreply.github.com>
2019-10-19 10:19:06 +0300

    Update 3.py

--- hw3/3.py
+++ hw3/3.py
@@ -1,8 +1,8 @@
 def fibbonachi(x)
-a = [0, 1]
-for i in range(x - 2):
-    t.append(a[-1] + a[-2])
-print(a)
+    a = [0, 1]
+    for i in range(x - 2):
+        t.append(a[-1] + a[-2])
+    print(a)
 
 n = int(input())
 fibbonachi(n)
