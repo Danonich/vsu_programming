3305a13e2cd25c493d3508a92903d97f4a8a18aa
Danonich <49477225+Danonich@users.noreply.github.com>
2019-10-20 16:44:19 +0300

    Update 1.py

--- hw3/1.py
+++ hw3/1.py
@@ -1,15 +1,20 @@
-import re
-ex = input('Example: ')
-s = ''.join(re.findall(r'[+, - ,*,/,%,**]', ex))
-index = ex.find(s)
-x, y = ex.split(s)
+ex = input()
+sym = {'+', '-', '*', '/', '//', '**'}
+op = ''.join(sym & set(ex))
+index = s.find(op)
+x, y = s[:index], s[index + 1:]
+
+if y.find(op) != -1:
+    op += op
+    y = s[index + 2:]
+
 x, y = float(x), float(y)
 op = {
-'+': x + y,
-'-': x - y,
-'/': x / y,
-'*': x * y,
-'//': x // y,
-'**': x ** y}
-print(op[s])
-
+    '+': x + y,
+    '-': x - y,
+    '*': x * y,
+    '/': x / y,
+    '//': x // y,
+    '**': x**y
+}
+print(op[op])
