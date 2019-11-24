8cf60e9f37b84c7313cb1a072f9c6d5568f62b5c
Danonich <49477225+Danonich@users.noreply.github.com>
2019-11-24 12:47:29 +0300

    Create odd_even.py

---
+++ Sort/odd_even.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def odd_even(data):
+    n = len(data)
+    is_sorted = 0
+    while is_sorted:
+        is_sorted = 1
+        for i in range(1, n - 1, 2):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+                is_sorted = 0
+        for i in range(n - 1, 2):
+            if data[i] > data[i + 1]:
+                data[i], data[i + 1] = data[i + 1], data[i]
+                is_sorted = 0
+    return data
+
+
+rand = [randint(0, 100) for i in range(15)]
+print(rand)
+odd_even(rand)
+print(rand)
