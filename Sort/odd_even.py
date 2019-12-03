8f7632b5f76d3dbad8c386553e049773f4f1695a
Danonich <49477225+Danonich@users.noreply.github.com>
2019-12-03 12:39:23 +0300

    Update odd_even.py

--- Sort/odd_even.py
+++ Sort/odd_even.py
@@ -4,13 +4,13 @@ from random import randint
 def odd_even(data):
     n = len(data)
     is_sorted = 0
-    while is_sorted == 0:
+    while not is_sorted:
         is_sorted = 1
         for i in range(1, n - 1, 2):
             if data[i] > data[i + 1]:
                 data[i], data[i + 1] = data[i + 1], data[i]
                 is_sorted = 0
-                  
+
         for i in range(0, n - 1, 2):
             if data[i] > data[i + 1]:
                 data[i], data[i + 1] = data[i + 1], data[i]
